a8a5471d0351adedb1d229be4ac2b0a9cd71f97a
Kevin Enario <kevin_enario@yahoo.com>
2023-10-09 09:19:16 -0700

    Removed unnecessary things from repo.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import { plugin, defaultConfig } from '@formkit/vue'
 import { createProPlugin, dropdown } from '@formkit/pro'
